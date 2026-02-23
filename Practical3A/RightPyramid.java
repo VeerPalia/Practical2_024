d40514a24aa3337576021fc9f863c55be72c7dec
VeerPalia <paliaveer111@gmail.com>
2026-02-23 19:11:24 +0530

    Updated Practical 3A

--- Practical3A/RightPyramid.java
+++ Practical3A/RightPyramid.java
@@ -1,21 +1,26 @@
-
-
 public class RightPyramid {
     public static void main(String[] args) {
 
-        int rows = 6;
-        for (int i = 1; i <= rows; i++) {
+        // Star Inverted Pyramid
+        for (int i = 6; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
-        for (int i = rows; i >= 1; i--) {
+
+        // Number Pattern
+        int num = 1;
+
+        for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print("* ");
+                System.out.print(num + " ");
+                num++;
+                if (num == 10) {
+                    num = 1;
+                }
             }
             System.out.println();
-    }
-        
+        }
     }
 }
--- Practical3A/sum.java
+++ Practical3A/sum.java
@@ -1,5 +1,6 @@
 public class sum {
     public static void main(String[] args) {
+
         int sum = 0;
 
         for (int i = 1; i <= 100; i += 2) {
